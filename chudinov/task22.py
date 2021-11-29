ed9f497f7da37189f979401fa82aa47fef49c6cf
Евгений Дмитриевич Чудинов <yevgeniychudinov@csu.ru>
2021-11-29 20:28:11 +0500

    init db

--- chudinov/task22.py
+++ chudinov/task22.py
@@ -1,13 +1,30 @@
+import sqlite3
+
 test = {}
+conn = sqlite3.connect(':memory:')
+c = conn.cursor()
+c.execute('''CREATE TABLE Good (Id integer PRIMARY KEY AUTOINCREMENT,Name string,ManufacturerId integer,Price float)''')
+c.execute('''CREATE TABLE Storage (Id integer, Count integer)''')
+c.execute('''CREATE TABLE Manufacturer (ManufacturerId integer PRIMARY KEY AUTOINCREMENT,PrName string,
+Country string)''')
+c.execute('''CREATE TABLE Size (Id integer,Size float)''')
 
 while True:
     input1 = str(input('>'))
     if input1 == "add":
         name = str(input('>>'))
         brand = str(input('>>'))
+        brandCountry = str(input('>>'))
         count = int(input('>>'))
-        size = int(input('>>'))
-        price = int(input('>>'))
+        size = float(input('>>'))
+        price = float(input('>>'))
+        c.execute('insert into Manufacturer(PrName, Country) values(?,?)', (brand, brandCountry))
+        c.execute('insert into Good(Name, ManufacturerId, Price) values (?,?,?)', (name, c.lastrowid, price))
+        c.execute('insert into Storage(Id, Count) values(?,?)', (c.lastrowid, count))
+        c.execute('insert into Size(Id, Size) values(?,?)', (c.lastrowid, size))
+
+        # Save (commit) the changes
+        conn.commit()
         test.update({name: {"brand": brand, "size": size, "price": price, "count": count}})
     elif input1 == "del":
         name = str(input('>>'))
@@ -31,4 +48,5 @@ while True:
         print(sizes)
         print(brands)
     else:
-        print(test)
+        c.execute('select * from Good JOIN Storage ON Good.Id = Storage.Id')
+        [print(row) for row in c.fetchall()]
